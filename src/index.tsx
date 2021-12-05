21f6e1221b35325097e80315bd3ed191855f03fa
Collin Pfeifer <collinpfeifer@Icloud.com>
2021-12-04 22:05:54 -0500

    added types and index

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>hello world</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+f
